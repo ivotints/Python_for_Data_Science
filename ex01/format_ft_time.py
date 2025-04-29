624929148d5fd4af733b212d7424b27b80ecf712
Igor Votintsev <ivotints@student.42prague.com>
2025-04-29 19:38:04 +0200

    small fi

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -4,5 +4,5 @@ import datetime
 cur_time = time.time()
 
 print(f"Seconds since January 1, 1970: {cur_time:,.4f}", end="")
-print(f"or {cur_time:.2e} in scientific notation")
+print(f" or {cur_time:.2e} in scientific notation")
 print(datetime.datetime.fromtimestamp(cur_time).strftime("%b %d %Y"))
