d434aa0b78095a292efeace45115da18e09910f1
Igor Votintsev <ivotints@student.42prague.com>
2025-04-29 19:34:20 +0200

    done

---
+++ ex09/README.md
@@ -0,0 +1,14 @@
+# ft_package
+
+A simple Python package created for educational purposes.
+
+## Usage
+
+```python
+from ft_package import count_in_list
+```
+
+# Count occurrences of an item in a list
+```
+result = count_in_list(["toto", "tata", "toto"], "toto")  # Returns: 2
+```
---
+++ ex09/ft_package/functions.py
@@ -0,0 +1,12 @@
+def count_in_list(lst, item_to_count):
+    """
+    Count the number of occurrences of item_to_count in lst
+
+    Args:
+        lst: List to search in
+        item_to_count: Item to count occurrences of
+
+    Returns:
+        Number of occurrences of item_to_count in lst
+    """
+    return lst.count(item_to_count)
